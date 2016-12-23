27fcb6d6764be0345dd51ec65a3ca9eb656de59a
Carlos Eduardo Lima Borges <carloslimaborges@gmail.com>
2016-12-22 23:55:57 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const status = require('http-status');
+
+status.CONTINUE;
+
+//in the line below, type the dot and see if the IDE shows the available options (ACCEPTED, BAD_GATEWAY and so on...)
+status
